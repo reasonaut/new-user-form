4ff1158ccce9f6b9c35295844ac857117bbf2a98
Jon Garrity <jonnygarrity@gmail.com>
2021-08-20 22:41:35 -0500

    Fixed multiple email validation error msg bug. Minor formatting changes.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import './style.css';
 
-
-
+const emailInput = document.getElementById('email-input');
 
 const validateEmail = (e) => {
     e.preventDefault();
     const addressToCheck = document.getElementById('email-input');
     if (!addressToCheck.checkValidity()) {
-        const notification = document.createElement('span');
-        notification.innerText = 'Email is malformed';
-        // insert notification span after input
-        addressToCheck.parentNode.insertBefore(notification, addressToCheck.nextSibling);
-    };
-}
+        if (emailInput.nextSibling.tagName.toLowerCase() !== 'span') {
+            const notification = document.createElement('span');
+            notification.innerText = 'Email is malformed';
+            // insert notification span after input
+            addressToCheck.parentNode.insertBefore(
+                notification,
+                addressToCheck.nextSibling
+            );
+        }
+    }
+};
 
 const submitButton = document.getElementById('submit-button');
+
+emailInput.addEventListener('focusin', () => {
+    if (emailInput.nextSibling.tagName.toLowerCase() === 'span')
+        emailInput.nextSibling.remove();
+});
 submitButton.addEventListener('click', validateEmail);
